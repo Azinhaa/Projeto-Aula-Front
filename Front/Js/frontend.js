adf1f6700bc5e42f808f50c25d07d73e9cc871e9
Carolina Mitsuoka <carol.mitsuoka@gmail.com>
2024-11-07 07:45:32 -0300

    aula 06/11

--- Front/Js/frontend.js
+++ Front/Js/frontend.js
@@ -1,6 +1,6 @@
+
 const protocolo= "http://"
 const baseURL= "localhost:3000"
-const filmesEndpoint = "/filmes"
 
 function listarFilmes(filmes){
     //atualizar a tabela 
@@ -18,8 +18,27 @@ function listarFilmes(filmes){
     }
 }
 
+function exibirAlerta (seletor, innerHTML, classesToAdd, classesToRemove, timeout){
+    let alert = document.querySelector(seletor)
+    alert.innerHTML = innerHTML
+    alert.classList.add(...classesToAdd)
+    alert.classList.remove(...classesToRemove)
+    setTimeout(() => {
+        alert.classList.remove(...classesToAdd)
+        alert.classList.add(...classesToRemove)
+    }, timeout)
+}
+
+function esconderModal(seletor, timeout){
+    setTimeout(() =>{
+        let modal = bootstrap.Modal.getInstance(document.querySelector(seletor))
+        modal.hide()
+    }, timeout)
+}
+
 
 async function obterFilmes(){
+    const filmesEndpoint = "/filmes"
     const URLcompleta= `${protocolo}${baseURL}${filmesEndpoint}`
     const filmes= (await axios.get(URLcompleta)).data
     listarFilmes(filmes)
@@ -29,6 +48,7 @@ async function obterFilmes(){
 }
 
 async function cadastrarFilme() {
+    const filmesEndpoint = "/filmes"
     // montar a url completa
     const URLcompleta= `${protocolo}${baseURL}${filmesEndpoint}`
     // busca nos inputs o que foi digitado
@@ -44,18 +64,117 @@ async function cadastrarFilme() {
         //enviando o filme novo e recebendo a base atualizada
         const filmes = (await axios.post(URLcompleta, {titulo, sinopse})).data
         listarFilmes(filmes)
+        
+
     } 
     else{
-        let alert = document.querySelector(".alert")
-        alert.classList.add("show")
-        alert.classList.remove("d-none")
-        setTimeout(() => {
-            alert.classList.remove("show")
-            alert.classList.add("d-none")
-        }, 2000)
+        exibirAlerta(".alert-filme", "Preencha todos os campos", ["show", "alert-danger"], ["d-none"], 2000)
+
+        // let alert = document.querySelector(".alert")
+        // alert.classList.add("show")
+        // alert.classList.remove("d-none")
+        // setTimeout(() => {
+        //     alert.classList.remove("show")
+        //     alert.classList.add("d-none")
+        // }, 2000)
+    }
+}
+
+async function cadastrarUsuario() {
+    let usuarioCadastroInput = document.querySelector("#usuarioCadastroInput")
+    let passwordCadastroInput = document.querySelector("#passwordCadastroInput")
+    let usuarioCadastro = usuarioCadastroInput.value
+    let passwordCadastro = passwordCadastroInput.value
+    if(usuarioCadastro && passwordCadastro){
+        try{
+            const cadastroEndpoint = "/signup"
+            const URLcompleta= `${protocolo}${baseURL}${cadastroEndpoint}`
+            await axios.post (URLcompleta,{login:usuarioCadastro, password:passwordCadastro})
+            
+
+            // let alert=document.querySelector(".alert-modal-cadastro")
+            // alert.innerHTML= "Cadastrado com sucesso!!!"
+            // alert.classList.add("show", "alert-success")
+            // alert.classList.remove("d-none")
+            // setTimeout(() => {
+            //     alert.classList.remove("show", "alert-success")
+            //     alert.classList.add("d-none")
+            //     let modalCadastro = bootstrap.Modal.getInstance(
+            //         document.querySelector("#modalCadastro")
+            //     )
+            //     modalCadastro.hide()
+            // }, 2000)
+
+            usuarioCadastroInput.value=""
+            passwordCadastroInput.value=""
+            exibirAlerta(".alert-modal-cadastro", "Usuario cadastrado com sucesso", ["show", "alert-success"], ["d-none"], 2000)
+            esconderModal("#modalCadastro", 2000)
+
+        }
+        catch(e){
+            usuarioCadastroInput.value=""
+            passwordCadastroInput.value=""
+            exibirAlerta(".alert-modal-cadastro", "Usuário já cadastrado", ["show", "alert-danger"], ["d-none"], 2000)
+            esconderModal("#modalCadastro", 2000)
+
+
+            // let alert=document.querySelector(".alert-modal-cadastro")
+            // alert.innerHTML= "Não foi possivel cadastrar!!!"
+            // alert.classList.add("show", "alert-danger")
+            // alert.classList.remove("d-none")
+            // setTimeout(() => {
+            //     alert.classList.remove("show", "alert-danger")
+            //     alert.classList.add("d-none")
+            //     let modalCadastro = bootstrap.Modal.getInstance(
+            //         document.querySelector("#modalCadastro")
+            //     )
+            //     modalCadastro.hide()
+            // }, 2000)
+        }
+    }
+    else{
+        exibirAlerta(".alert-modal-cadastro", "Preencha todos os campos", ["show", "alert-danger"], ["d-none"], 2000)
+
+        // let alert=document.querySelector(".alert-modal-cadastro")
+        // alert.innerHTML= "Preencha todos os campos!!!"
+        // alert.classList.add("show", "alert-danger")
+        // alert.classList.remove("d-none")
+        // setTimeout(() => {
+        //     alert.classList.remove("show", "alert-danger")
+        //     alert.classList.add("d-none")
+        // }, 2000)
     }
 }
 
 // {
 //     console.log("teste")
 // }
+
+const loginUsuario = async() => {
+    let usuarioLoginInput = document.querySelector("#usuarioLoginInput")
+    let passwordLoginInput = document.querySelector("#passwordLoginInput")
+    let usuarioLogin = usuarioLoginInput.value
+    let passwordLogin = passwordLoginInput.value
+    if(usuarioLogin&&passwordLogin){
+        try{
+            const loginEndpoint= "/login"
+            const URLcompleta = `${protocolo}${baseURL}${loginEndpoint}`
+            const response= await axios.post(
+                URLcompleta,
+                {login:usuarioLogin, password:passwordLogin})
+
+            usuarioLoginInput.value=""
+            passwordLoginInput.value=""
+            exibirAlerta(".alert-modal-login", "Login Realizado com sucesso", ["show", "alert-success"], ["d-none"], 2000)
+            esconderModal("#modalLogin", 2000)
+            const cadastrarFilmeButton=document.querySelector("#cadastrarFilmeButton")
+            cadastrarFilmeButton.disabled = false
+        }
+        catch(e){
+
+        }
+    }
+    else{
+        exibirAlerta(".alert-modal-login", "preencha todos os campos", ["show", "alert-danger"],["d-none"],2000)
+    }
+}
--- backend.js
+++ backend.js
@@ -2,6 +2,9 @@
 const express = require ('express')
 const cors= require('cors')
 const mongoose = require("mongoose")
+const uniqueValidator = require("mongoose-unique-validator")
+const bcrypt= require("bcrypt")
+const jwt= require("jsonwebtoken")
 const app = express()
 app.use(express.json())
 app.use(cors())
@@ -16,6 +19,18 @@ const Filme = mongoose.model("Filme", mongoose.Schema({
     titulo: {type: String},
     sinopse: {type: String}
 }))
+
+
+// construindo o login
+const usuarioSchema = mongoose.Schema({
+    login: {type: String, required: true, unique: true},
+    password: { type: String, required:true}
+})
+
+usuarioSchema.plugin(uniqueValidator)
+const Usuario = mongoose.model("Usuario", usuarioSchema)
+
+
 
 // variaveis
 // const = constsnte
@@ -43,6 +58,7 @@ app.get("/filmes", async (req, res) => {
     res.json(filmes)
 })
 
+
 app.post("/filmes", async (req, res) =>{
     //capturar o que user enviar
     const titulo= req.body.titulo;
@@ -53,6 +69,46 @@ app.post("/filmes", async (req, res) =>{
     const filmes = await Filme.find()
     res.json(filmes)
 })
+
+
+
+app.post("/signup", async(req, res) => {
+    try{
+        const login = req.body.login
+        const password=  req.body.password
+        const senhaCriptografada= await bcrypt.hash(password, 10)
+        const usuario= new Usuario({login:login, password: senhaCriptografada})
+        const respMongo= await usuario.save()
+        console.log(respMongo)
+        res.status(201).end()
+    }
+    catch (e){
+        console.log(e)
+        res.status(409).end()
+    }
+
+})
+
+app.post("/login", async(req, res)=> { 
+    const login= req.body.login
+    const password = req.body.password
+    const usuarioExiste = await Usuario.findOne({login: login})
+    if (!usuarioExiste){
+        return res.status(401).json({mensagem: "login invalido"})
+    }
+    const senhaInvalida= await bcrypt.compare(password, usuarioExiste.password)
+    if (!senhaInvalida){
+        return res.status(401).json({mensagem: "senha invalida"})
+    }
+
+    const token = jwt.sign(
+        {login: login}, 
+        "chave secreta",
+        {expiresIn: "1h"}
+    )
+    res.status(200).json({token: token})
+})
+
 
 async function conectarAoBanco(){
     await mongoose.connect(`mongodb+srv://carolmitsuoka:12345678910@cluster0.ixl1x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
